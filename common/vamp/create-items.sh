2efebfe173ee7935f78b949eafb909bff3b498ad
Stuart Leeks <stuart@leeks.net>
2017-07-07 06:37:15 +0100

    And common vamp scripts

---
+++ common/vamp/create-items.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+BASE_URL="http://vamp.faux.ninja:8080"
+DEPLOYMENT_FILE=""
+
+function show_usage(){
+    echo "create-items"
+    echo
+    echo -e "\t--base-url\t(Optional) Specify the base vamp url [default $BASE_URL]"
+    echo -e "\t--deployment-file | -f\tThe file containing the items to deploy"
+}
+
+while [[ $# -gt 0 ]]
+do
+    case "$1" in 
+        --base-url)
+            BASE_URL="$2"
+            shift 2
+            ;;
+        --deployment-file | -f)
+            DEPLOYMENT_FILE="$2"
+            shift 2
+            ;;
+        *)
+            show_usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $DEPLOYMENT_FILE ]; then
+    echo "deployment file not specified"
+    echo
+    show_usage
+    exit 1
+fi
+
+
+# Deployment file should be
+# ---
+# kind: breed
+# name: name-here
+# ...
+#
+# ---
+# kind: <next kind>
+# ...
+
+# see vamp runner recipes for examples
+
+curl    -X PUT \
+        -H 'Content-Type: application/x-yaml' \
+        -H 'Accept: application/x-yaml' \
+        --data-binary @$DEPLOYMENT_FILE \
+        "$BASE_URL/api/v1/"
