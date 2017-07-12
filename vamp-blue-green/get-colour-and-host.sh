5a97e8a7db4a9a00801d0612e836d4bbcaaa89d7
Stuart Leeks <stuart@leeks.net>
2017-07-12 14:59:23 +0100

    Add (optional) cookie handling

--- vamp-blue-green/get-colour-and-host.sh
+++ vamp-blue-green/get-colour-and-host.sh
@@ -8,8 +8,38 @@ blue="\e[1;34m"
 green="\e[1;32m"
 no_colour="\e[1;0m"
 
+
+COOKIES=false
+
+function show_usage(){
+    echo "get-colour-and-host"
+    echo
+    echo -e "\t--cookies\tpreserve cookies across requests"
+}
+
+while [[ $# -gt 0 ]]
+do
+    case "$1" in 
+        --cookies)
+            COOKIES=cookies
+            shift 1
+            ;;
+        *)
+            echo "Unexpected '$1'"
+            show_usage
+            exit 1
+            ;;
+    esac
+done
+
+rm -f /tmp/bluegreencookies.txt
+
 while [ 1 -eq 1 ]; do
-    curl $url -s -o /tmp/bluegreen.html
+    if [ "$COOKIES" == cookies ]; then
+        curl $url -s -b /tmp/bluegreencookies.txt --cookie-jar /tmp/bluegreencookies.txt -o /tmp/bluegreen.html
+    else
+        curl $url -s -o /tmp/bluegreen.html
+    fi
 
     colour=$( grep -Po "(?<=background-color: )(?:[a-z0-9-]*)(?=;)"  /tmp/bluegreen.html)
     host=$(grep -Po "(?<=>)(?:[a-z0-9-]*)(?=</body)"  /tmp/bluegreen.html)
