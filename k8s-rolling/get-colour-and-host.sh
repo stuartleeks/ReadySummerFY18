ad256c83d0c42d08b543c9ebb4d1852da129d214
Stuart Leeks <stuart@leeks.net>
2017-07-13 14:04:36 +0100

    Make get-colour... script more resilient

--- k8s-rolling/get-colour-and-host.sh
+++ k8s-rolling/get-colour-and-host.sh
@@ -6,16 +6,53 @@ url=http://rolling.faux.ninja/
 
 blue="\e[1;34m"
 green="\e[1;32m"
+red="\e[1;31m"
 no_colour="\e[1;0m"
 
+COOKIES=false
+
+function show_usage(){
+    echo "get-colour-and-host"
+    echo
+    echo -e "\t--cookies\tpreserve cookies across requests"
+}
+
+while [[ $# -gt 0 ]]
+do
+    case "$1" in 
+        --cookies)
+            COOKIES=cookies
+            shift 1
+            ;;
+        *)
+            echo "Unexpected '$1'"
+            show_usage
+            exit 1
+            ;;
+    esac
+done
+
+rm -f /tmp/rollingcookies.txt
+
 while [ 1 -eq 1 ]; do
-    curl $url -s -o /tmp/bluegreen.html
 
-    colour=$( grep -Po "(?<=background-color: )(?:[a-z0-9-]*)(?=;)"  /tmp/bluegreen.html)
-    host=$(grep -Po "(?<=>)(?:[a-z0-9-]*)(?=</body)"  /tmp/bluegreen.html)
+    if [ "$COOKIES" == cookies ]; then
+        curl $url -i -s -b /tmp/rollingcookies.txt --cookie-jar /tmp/rollingcookies.txt -o /tmp/rolling.html
+    else
+        curl $url -i -s -o /tmp/rolling.html
+    fi
 
-    ansiColour=${!colour}
-    echo -e "${ansiColour}Host: $host   Colour: $colour$no_colour"
+    status=$(grep -Po "(?<=HTTP/1.[01] )(?:[0-9]*)" /tmp/rolling.html)
+    if [ "$status" == "200" ]; then
+        colour=$(grep -Po "(?<=background-color: )(?:[a-z0-9-]*)(?=;)"  /tmp/rolling.html)
+        host=$(grep -Po "(?<=>)(?:[a-z0-9-]*)(?=</body)"  /tmp/rolling.html)
+
+        ansiColour=${!colour}
+        echo -e "${ansiColour}Host: $host   Colour: $colour$no_colour"
+    else
+        message=$(grep -Po "(?<=<h1>)(?:.*)(?=</h1>)" /tmp/rolling.html )
+        echo -e "$red$message$no_colour"
+    fi
     # sleep 0.1
 done
 
--- vamp-blue-green/get-colour-and-host.sh
+++ vamp-blue-green/get-colour-and-host.sh
@@ -6,6 +6,7 @@ url=http://bluegreen.faux.ninja/
 
 blue="\e[1;34m"
 green="\e[1;32m"
+red="\e[1;31m"
 no_colour="\e[1;0m"
 
 
@@ -36,16 +37,22 @@ rm -f /tmp/bluegreencookies.txt
 
 while [ 1 -eq 1 ]; do
     if [ "$COOKIES" == cookies ]; then
-        curl $url -s -b /tmp/bluegreencookies.txt --cookie-jar /tmp/bluegreencookies.txt -o /tmp/bluegreen.html
+        curl $url -i -s -b /tmp/bluegreencookies.txt --cookie-jar /tmp/bluegreencookies.txt -o /tmp/bluegreen.html
     else
-        curl $url -s -o /tmp/bluegreen.html
+        curl $url -i -s -o /tmp/bluegreen.html
     fi
 
-    colour=$( grep -Po "(?<=background-color: )(?:[a-z0-9-]*)(?=;)"  /tmp/bluegreen.html)
-    host=$(grep -Po "(?<=>)(?:[a-z0-9-]*)(?=</body)"  /tmp/bluegreen.html)
+    status=$(grep -Po "(?<=HTTP/1.[01] )(?:[0-9]*)" /tmp/bluegreen.html)
+    if [ "$status" == "200" ]; then
+        colour=$(grep -Po "(?<=background-color: )(?:[a-z0-9-]*)(?=;)"  /tmp/bluegreen.html)
+        host=$(grep -Po "(?<=>)(?:[a-z0-9-]*)(?=</body)"  /tmp/bluegreen.html)
 
-    ansiColour=${!colour}
-    echo -e "${ansiColour}Host: $host   Colour: $colour$no_colour"
+        ansiColour=${!colour}
+        echo -e "${ansiColour}Host: $host   Colour: $colour$no_colour"
+    else
+        message=$(grep -Po "(?<=<h1>)(?:.*)(?=</h1>)" /tmp/bluegreen.html )
+        echo -e "$red$message$no_colour"
+    fi
     sleep 0.5
 done
 
