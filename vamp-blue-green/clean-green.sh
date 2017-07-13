58897e27a65fe1c930b4a469539cd1949f78e0fd
Stuart Leeks <stuart@leeks.net>
2017-07-13 14:04:17 +0100

    Fix issue in quick reset

--- vamp-blue-green/clean-green.sh
+++ vamp-blue-green/clean-green.sh
@@ -4,5 +4,5 @@ SCRIPT=$(readlink -f $0)
 BASE_DIR=`dirname ${SCRIPT}`
 
 
-"$BASE_DIR/set-gateway-blue.sh"
+"$BASE_DIR/../common/vamp/create-items.sh" -f "$BASE_DIR/definitions/gateway-blue-only.yml"
 "$BASE_DIR/../common/vamp/delete-items.sh" -f "$BASE_DIR/definitions/clean-green.yml"
