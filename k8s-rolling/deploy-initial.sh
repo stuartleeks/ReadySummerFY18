de8121c7431a0c637b944c4b28bb73641fb8b09a
Stuart Leeks <stuart@leeks.net>
2018-02-01 06:56:12 -0800

    Add dns setup in deploy.initial.sh

--- k8s-rolling/deploy-initial.sh
+++ k8s-rolling/deploy-initial.sh
@@ -3,4 +3,9 @@
 SCRIPT=$(readlink -f $0)
 BASE_DIR=`dirname ${SCRIPT}`
 
-kubectl create -f "$BASE_DIR/definitions/rolling.yml"
+kubectl apply -f "$BASE_DIR/definitions/rolling.yml"
+
+if [ ! -z $DEMO_DOMAIN_NAME ]; then
+    echo "setting dns..."
+    $BASE_DIR/../common/dns/set-dns.sh --domain $DEMO_DOMAIN_NAME --subdomain rolling --service-name rolling
+fi
--- vamp-blue-green/deploy-initial.sh
+++ vamp-blue-green/deploy-initial.sh
@@ -4,3 +4,9 @@ SCRIPT=$(readlink -f $0)
 BASE_DIR=`dirname ${SCRIPT}`
 
 "$BASE_DIR/../common/vamp/create-items.sh" -f "$BASE_DIR/definitions/deploy-initial.yml"
+
+
+if [ ! -z $DEMO_DOMAIN_NAME ]; then
+    echo "setting dns..."
+    $BASE_DIR/../vamp-blue-green/set-dns-for-gateway.sh --domain $DEMO_DOMAIN_NAME --subdomain bluegreen --gateway-name demo_80 
+fi
